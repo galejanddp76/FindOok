3de0fb18219e829e1aa36125bb02053921fc953b
galejanddp76 <galejanddp76@educastur.es>
2021-05-31 20:09:57 +0200

    Mejora de nav

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -4,25 +4,16 @@ window.onload = function() {
     //crea los elementos
     const contenedor = document.createElement("div");
     const publicado = document.createElement("section");
-    const carrito = document.createElement("section");
 
     //crea las clases de los elementos
     contenedor.classList.add("contenedor");
     publicado.classList.add("publicaciones");
-    carrito.classList.add("carrito");
-    carrito.style.right = "-400px";
 
     //crea la estructura de hijos y padres
     document.body.appendChild(contenedor);
     contenedor.appendChild(publicado);
-    contenedor.appendChild(carrito);
 
-    //crea el header del carrito
-    const headerCarrito = document.createElement("header");
-    headerCarrito.innerHTML = "<h3>Productos en carrito</h3>";
-    carrito.appendChild(headerCarrito);
-
-leerPublicacion();
+	leerPublicacion();
 
     async function leerPublicacion() {
         const response = await fetch('https://findookapp.herokuapp.com/publicaciones');
@@ -49,71 +40,5 @@ leerPublicacion();
         </article>
     `
         }
-        publicado.innerHTML = detalle;
-    
-        const botones = document.querySelectorAll(".boton");
-
-        for (const boton of botones) {
-            boton.addEventListener("click", ponerCarrito);
-
-        }
-        
-        function ponerCarrito(even) {
-
-            let detallesCompra = "";
-            let evento = even.target;
-
-            // Crea botón de eliminar
-            let itemsCarrito = document.createElement("div");
-            itemsCarrito.innerHTML = "<span class='eliminar'>X</span>";
-            itemsCarrito.classList.add("elimina-compra");
-            carrito.appendChild(itemsCarrito);
-
-            //crea la imagen
-            let imgCompra = document.createElement("img");
-            imgCompra.classList.add("imagen")
-            imgCompra.src = evento.parentNode.children[1].src;
-            imgCompra.width = 150;
-            itemsCarrito.appendChild(imgCompra);
-
-            // Crea elementos detalles
-            let compra = document.createElement("div");
-            compra.classList.add("detallesCompra");
-            detallesCompra += `
-        <div class="detalles">
-            <p>${evento.parentNode.children[0].textContent}</p>
-            <p>${evento.parentNode.children[2].children[0].textContent}</p>
-            <p>${evento.parentNode.children[2].children[1].textContent}</p>
-            <p>${evento.parentNode.children[2].children[2].textContent}</p>
-        </div>
-        `
-            compra.innerHTML = detallesCompra;
-            itemsCarrito.appendChild(compra);
-
-            // Crea botón de compra
-            itemsCarrito.innerHTML += "<span class='comprar'>Comprar</span>";
-
-
-            // Evento para eliminar compra del carrito
-            itemsCarrito.firstChild.addEventListener("click", eliminaCompra);
-            itemsCarrito.lastChild.addEventListener("click", eliminaCompra);
-
-            //Función para eliminar compra
-            function eliminaCompra(even) {
-                let elementoSeleccionado = even.target.parentNode;
-                elementoSeleccionado.remove();
-            }
-        }
-        
-        document.querySelector(".carro").addEventListener("click", muestraCarrito);
-		
-		    // funcion que muestra el carrito
-    	function muestraCarrito() {
-        	if (carrito.style.right == "-400px") {
-            	carrito.style.right = 0;
-        	} else {
-            	carrito.style.right = "-400px";
-        }
-    }
     }
  }
