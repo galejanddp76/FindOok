89ed3d2ff6b6edf4bd958058567bb753966e45f9
galejanddp76 <galejanddp76@educastur.es>
2021-06-08 22:50:29 +0200

    mejora ver publicacion

--- src/main/resources/static/js/verPublicacion.js
+++ src/main/resources/static/js/verPublicacion.js
@@ -2,17 +2,7 @@ window.onload = function() {
 	const id = getQueryVariable("idpublicacion");
     
     //crea los elementos
-    const publicado = document.createElement("div");
-    const comentado = document.createElement("div");
-
-    //crea las clases de los elementos
-    publicado.classList.add("publicacion");
-    comentado.classList.add("comentarios");
-
-    //crea la estructura de hijos y padres
-    document.body.appendChild(publicado);
-    document.body.appendChild(comentado);
-
+   
 	leerPublicacion();
 	//lee la publicacion de la base de datos
     async function leerPublicacion() {
@@ -27,20 +17,25 @@ window.onload = function() {
         let detalles = `
         <div class="usuimg">
         	<img src="${publicacion.imagenpublicacion}" alt="${publicacion.titulo}"/>
-        	<div class="usu"><img src="/images/perfilDefault.png" alt="usuario"><strong>${publicacion.usuario.username}</strong></div>
+        	<div class="contacto">
+        		<div><strong>CONTACTO</strong></div>
+        		<div><strong>Usuario:</strong> ${publicacion.usuario.username}</div>
+        		<div><strong>Correo:</strong> ${publicacion.usuario.correo}</div>
+        	</div>
         </div>
-            <div class="contenedor">
+            <div class="detalles">
                 <div class="titulo"><h2>${publicacion.titulo}</h2></div>
                 <div class="info">
                 	<div class="descripcion"><p>${publicacion.descripcion}</p></div>
-                	<div class="precio"><strong>Precio:</strong>${publicacion.precio}€</div>
+                	<div class="precio"><strong>Precio:</strong> ${publicacion.precio}€</div>
                 	<div class="botones">
             			<a href="/intercambio">Intercambiar</a>
             			<a href="/pago">Comprar</a>
            			</div>
             	</div>
-            </div>`;
-            publicado.innerHTML = detalles;
+            </div>`
+            ;
+            document.querySelector(".publicacion").innerHTML = detalles;
         }
         
     }
