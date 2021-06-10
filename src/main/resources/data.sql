72d8731d876ed969a3dcc9d5515d91fc1ed590c0
galejanddp76 <galejanddp76@educastur.es>
2021-06-10 23:11:27 +0200

    arreglo de publicacion

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,5 +4,3 @@ insert into roles(nombrerol) values("ROLE_ADMIN")
 insert into roles(nombrerol) values("ROLE_USER")
 
 insert into usuariosroles(idrol,idusuario) values(1,1)
-
-insert into publicaciones(idpublicacion,titulo,descripcion,imagenpublicacion,fechacreacion,precio,idusuario) values(1,"Prueba","Esto es una prueba","/images/logo_small.png","2001-11-29",3,1)
